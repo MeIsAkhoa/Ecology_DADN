6c64858d25594f07bc34b7acaf894157956a693c
Khoa1204HCMUT <156278110+mp777888@users.noreply.github.com>
2025-04-22 00:31:10 +0700

    change baseurl for deploy

--- src/utils/baseURL.ts
+++ src/utils/baseURL.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Tạo instance axios với baseURL cố định
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
